36f4a84122e3fb5cc0b4daff5730acd221de54f2
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-24 00:03:58 -0300

    Update o).py

--- Questao_04_Implementação_dos_problemas_pedidos/o).py
+++ Questao_04_Implementação_dos_problemas_pedidos/o).py
@@ -1,5 +1,5 @@
-print ("o) Resultado variável: ")
-
+# o) Resultado variável:
+print ("Digite os valores variáveis: ")
 A = int(input("Insira a variável A: "))
 B = int(input("Insira a variável B: "))
 C = int(input("Insira a variável C: "))
@@ -9,3 +9,4 @@ variávelS = B + D
 print (f" A variável P é: {variávelP}")
 print (f" A variável S é: {variávelS}")
 print("="*40)
+
