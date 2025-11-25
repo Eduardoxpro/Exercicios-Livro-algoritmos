d65af069b1d0cb606fbba4807bfb8dc005157754
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-25 10:46:41 -0300

    Add files via upload

---
+++ Questao_04_Implementação_dos_problemas_pedidos/y).py
@@ -0,0 +1,8 @@
+
+# y) Antecessor e sucessor
+
+print("y) Antecessor e sucessor")
+n = int(input("Digite um número inteiro: "))
+print(f"Antecessor: {n-1}")
+print(f"Número: {n}")
+print(f"Sucessor: {n+1}")
