64ba8f82c271c10991b4759119dfae75293d4400
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-24 22:17:38 -0300

    Add files via upload

---
+++ Questao_04_Implementação_dos_problemas_pedidos/q).py
@@ -0,0 +1,16 @@
+# q) Área da circunferência
+
+import math
+
+raio = float(input("Digite o valor do raio: "))
+area = math.pi * raio ** 2
+print(f"O valor da circunferência é: {area:.2f}")
+
+# ─────────────────────────────────────
+# q) Área da circunferência
+# ─────────────────────────────────────
+import math
+print("\nq) Área da circunferência")
+raio = float(input("Digite o raio: "))
+area = math.pi * raio ** 2
+print(f"A área da circunferência é {area:.4f}")
