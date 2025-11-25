6873e1706157a265472fc4ddbd5a5e3beea612c7
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-25 10:46:23 -0300

    Update e).py

---
+++ Questao_04_Implementação_dos_problemas_pedidos/e).py
@@ -0,0 +1,9 @@
+# Calculo DE JUROS SIMPLES
+
+print ("e) Calculo de bem em atraso: ")
+valor = float(input("Digite o valor da prestação: "))
+taxa = float(input("Digite a taxa da prestação: "))
+tempo = float(input("Digite o tempo de atraso em meses:  "))
+prestacao = valor + (valor * (taxa / 100) * tempo)
+print (f"O valor total da prestação com juros é: {prestacao:.2f} R$ ")
+print("="*40)
