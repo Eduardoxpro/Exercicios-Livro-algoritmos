97d493ac8f88ba24582c710914b6d7102c48b7bd
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-24 22:19:47 -0300

    Delete p).py

--- p).py
+++
@@ -1,7 +0,0 @@
-
-print ("p) Salario mensal: ")
-
-Salariomensal = float(input("Digite o valor do seu salário mensal: "))
-PercentualReajuste = float(input("Digite a porcentagem de reajuste salarial: "))
-Novosalario = PercentualReajuste * Salariomensal / 100 + Salariomensal
-print (f"O valor do salário atualizado é: {Novosalario:.2f}")
