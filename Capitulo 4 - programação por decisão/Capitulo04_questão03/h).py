9bbcd7099756b7b43567460c4e83bb48eaa870f2
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2025-11-30 23:45:25 -0300

    Update h).py

---
+++ Capitulo 4 - programação por decisão/Capitulo04_questão03/h).py
@@ -0,0 +1,9 @@
+# h) Faça um programa que leia 5 números e informe a soma e a média dos números.
+
+numeros = [float(input(f"Digite o {i+1}º número: ")) for i in range(5)]
+
+soma = sum(numeros)
+media = soma / 5
+
+print(f"Soma: {soma}")
+print(f"Média: {media:.2f}")
