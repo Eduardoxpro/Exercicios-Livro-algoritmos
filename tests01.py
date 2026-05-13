8ed3ba690d2adfa532c2736d7432cd1f40270946
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2026-05-13 20:56:14 -0300

    Delete tests01.py

--- tests01.py
+++
@@ -1,4 +0,0 @@
-numero01 = int(input ('digite o primeiro numero: '))
-numero02 = int(input ('digite o segundo numero: '))
-numerofinal = numero01 + numero02
-print ('O valor das somas de ambos numero é: ', numerofinal)
