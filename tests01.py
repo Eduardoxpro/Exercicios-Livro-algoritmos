39bf624ced37a9f5ea7ab7d36cf6f11f4f3337e9
Eduardo Rodrigues <78939657+Eduardoxpro@users.noreply.github.com>
2026-05-13 20:55:21 -0300

    teste

---
+++ tests01.py
@@ -0,0 +1,4 @@
+numero01 = int(input ('digite o primeiro numero: '))
+numero02 = int(input ('digite o segundo numero: '))
+numerofinal = numero01 + numero02
+print ('O valor das somas de ambos numero é: ', numerofinal)
